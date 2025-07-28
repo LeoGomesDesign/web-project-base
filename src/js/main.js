005838c728689dfecf02698ac2867a81e3343ad5
LeoGomesDesign <leogomes.design@gmail.com>
2025-07-28 17:02:44 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,31 @@
+// Importar módulos
+import Navigation from './modules/navigation.js';
+import FormHandler from './modules/forms.js';
+import { debounce, throttle } from './utils/helpers.js';
 
+// Inicializar aplicação
+class App {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    // Aguardar DOM carregar
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', () => this.initializeComponents());
+    } else {
+      this.initializeComponents();
+    }
+  }
+
+  initializeComponents() {
+    // Instanciar componentes
+    new Navigation();
+    new FormHandler();
+    
+    console.log('🚀 Aplicação inicializada!');
+  }
+}
+
+// Inicializar
+new App();
